d55da3f4eb40e0e3b762edcc22feee828fd60385
Deepak Labh <labh111349@gmail.com>
2017-05-14 10:06:56 +0530

    vector without tokenizing sentence

--- NLPutilsDL/doc2vec.py
+++ NLPutilsDL/doc2vec.py
@@ -12,7 +12,7 @@ class data:
 	self.file_type = file_type
 	self.collection = collection
 
-    def create_vectors(self, sentence_field, word_key, vec_key, max_sent_len = 40, max_num_sents = 10, wordvec_dim = 300):
+    def create_vectors(self, sentence_field, word_key, vec_key, max_sent_len = 40, max_num_sents = 10, wordvec_dim = 300, sent_tokenize_flag = True):
         ''' --->> create_vectors('text', '_id', 'vec', 10, 5, 300)
 	    --->> collection.find_one({woed_key:vec_key})[vec_key] --> mongo collection object query fields	'''
 	zero_vec_sent = np.zeros((max_sent_len,wordvec_dim))
@@ -23,7 +23,8 @@ class data:
 	    import json
 	    data = json.load(open(self.file_path)) 
 	    for i in tqdm(xrange(len(data))):
-                sents = sent_tokenize(json.loads(data[i])[sentence_field].encode('ascii','ignore').lower().strip())
+		paragraph = json.loads(data[i])[sentence_field].encode('ascii','ignore').lower().strip()
+                sents = sent_tokenize(paragraph) if sent_tokenize_flag else [paragraph]
                 para_vec = []
                 for sent in sents:
                     words = sent.strip().split()
@@ -37,7 +38,8 @@ class data:
                     sent_vec = sent_vec[:max_sent_len] if len(sent_vec)>max_sent_len else sent_vec+[zero_vec]*(max_sent_len-len(sent_vec))
                 
                     para_vec.append(sent_vec)
-                para_vec = para_vec[:max_num_sents] if len(para_vec)>max_num_sents else para_vec+[zero_vec_sent]*(max_num_sents-len(para_vec))
+		if not sent_tokenize_flag: para_vec = para_vec[0]
+                else: para_vec = para_vec[:max_num_sents] if len(para_vec)>max_num_sents else para_vec+[zero_vec_sent]*(max_num_sents-len(para_vec))
                 x.append(para_vec)
 	else: raise Exception('only json format supported till now')
 	return np.array(x)
